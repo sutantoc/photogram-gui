7653244e977f4e822a5d301bf6c6bff9ff251ec6
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-16 21:45:37 +0000

    user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,15 @@ class UsersController < ApplicationController
 
     redirect_to("/users")
   end
+
+  def show
+    the_username = params.fetch("username")
+    matching_users = User.where({ :username => the_username})
+    @the_user = matching_users.at(0)
+
+    user_photos = @the_user.own_photos.all
+    @list_of_photos = user_photos.order({ :posted => :desc })
+    
+    render({ :template => "user_templates/show" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ get("/", { :controller => "users", :action => "index"})
 
 get("/users", { :controller => "users", :action => "index"})
 post("/add_user", { :controller => "users", :action => "create"})
+get("/users/:username", { :controller => "users", :action => "show"})
 
 end
