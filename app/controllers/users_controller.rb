b828827ec0888208f515b58727205382dc8da8f3
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-16 21:23:23 +0000

    create index base page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
-    render({ :template => "/user_templates/index"})
+    all_users = User.all
+    @list_of_users = all_users.order({ :created_at => :desc })
+
+    render({ :template => "/user_templates/index" })
   end
 end
